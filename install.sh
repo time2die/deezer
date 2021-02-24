af71618925116a187d09ff6a5a2c0286c405cae6
siphomateke <siphomateke@gmail.com>
2021-02-24 16:51:39 +0200

    v4.32.20

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Maintainer: Sibren Vasse <arch@sibrenvasse.nl>
 # Contributor: Ilya Gulya <ilyagulya@gmail.com>
 pkgname="deezer"
-pkgver=4.32.0 
+pkgver=4.32.20 
 srcdir="$PWD"
 
 install_dependencies() {
