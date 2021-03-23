a7b740b4b4b43ef4fdbf6b7c075bbc56b7573a8e
Daltro Augusto <daltro.campanher@acad.ufsm.br>
2021-03-23 07:20:26 -0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -9,7 +9,7 @@ sudo rm /usr/share/applications/deezer.desktop &> /dev/null || true
 sudo rm /usr/bin/deezer &> /dev/null || true
 
 for size in 16 32 48 64 128 256; do
-    sudo rm /usr/share/icons/hicolor/${size}x${size}/apps/deezer.png
+    sudo rm /usr/share/icons/hicolor/${size}x${size}/apps/deezer.png &> /dev/null || true
 done
 
 sudo update-desktop-database --quiet
